d923e38aca2cb6c8b128a8e1979996c426a2508d
Jenn <jenniferjuarezjj@gmail.com>
2023-10-04 16:42:11 -0400

    create first method

--- BookStore.java
+++ BookStore.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
+
+import java.util.List;
+
 public class BookStore {
+    public BookStore(){
+        inventory = new ArrayList<>();
+
+    }
+
+
+
+
     public static void main(String[] args) {
 
 
