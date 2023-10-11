f81b32d829b652659f8a69d56cec1123edeadd15
Jenn <jenniferjuarezjj@gmail.com>
2023-10-11 14:48:48 -0400

    created methods and variables for the BookStore and customer class

--- BookStore.java
+++ BookStore.java
@@ -1,5 +1,4 @@
-
-
+import java.util.Scanner;
 
 public class BookStore {
     String nameOfBook;
@@ -9,7 +8,24 @@ public class BookStore {
     String ISBN;
     String publishDate;
     String price;
-    static class Book {
+    public BookStore() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter name of book: ");
+        this.nameOfBook = in.nextLine();
+        System.out.print("Enter name of author's first name: ");
+        this.authorFirstName = in.nextLine();
+        System.out.print("Enter name of author's last name: ");
+        this.authorLastName = in.nextLine();
+        System.out.print("Enter what genre this book is: ");
+        this.genre = in.nextLine();
+        System.out.print("Enter the book's ISBN #: ");
+        this.ISBN = in.nextLine();
+        System.out.print("Enter when the book was published: ");
+        this.publishDate = in.nextLine();
+        System.out.print("Enter how much the book costs: ");
+        this.price = in.nextLine();
+        System.out.println(nameOfBook + " " + authorFirstName + " " + authorLastName + " " + genre + " " + ISBN
+                + " " + publishDate + " " + price + " ");
 
 
     }
--- Main.java
+++ Main.java
@@ -3,5 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         Customer cus1 = new Customer();
+        BookStore book1 = new BookStore();
         }
+
 }
